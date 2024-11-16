cde048ae7f1d79cb49d8a304e2502b7d464a807b
Bartłomiej <mozartwa123@gmail.com>
2024-11-16 13:02:03 +0100

    Dodano angielskie komentarze do pier_ur.c

--- pier_ur.c
+++ pier_ur.c
@@ -22,14 +22,14 @@ double sqrt(double n)//implementacja algorytmu Newtona-Raphsona dla pierwiastka 
 int main(int argc, char * argv[])
 {
     if(argc<3) return 0;
-    double wpsa = 1;
+    double wpsa = 1;//wpsa=1 means real part of number to root is bigger that zero, wpsa=-1 means real part is smaller than zero
     if(argv[1][0] == '-') {argv[1][0] = '0'; wpsa = -1;}
     double wpsb = 1;
     double a = wpsa * atof(argv[1]);
     if(argv[2][0] == '-') {argv[2][0] = '0'; wpsb = -1;}
     double b = wpsb * atof(argv[2]);
-    double modul = sqrt((a*a) + (b*b));
-    double sin = sqrt((modul-a)/(2*modul));
+    double modul = sqrt((a*a) + (b*b));//calculating module
+    double sin = sqrt((modul-a)/(2*modul));//calculating argument of rooted number (from formula calculating cosinus of doubled angle)
     double cos = sqrt(1 - (modul-a)/(2*modul));
     double real = sqrt(modul) * cos;
     double imag = sqrt(modul) * sin;
